6345e9299024832b4c5e5d74b263df20c1e971c5
dawtom97 <daw.tomczyk1997@gmail.com>
2025-07-09 17:21:48 +0200

    repo.py file created

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 title = "Mój nowy plik"
 x = 5
+
+y = "nowy fragment kodu"
+
 # Tworzenie lokalnego repozytorium
 # git init
 
@@ -11,3 +14,7 @@ x = 5
 # Dodawanie pliku do repozytorium - lokalnego
 # git add nazwa_pliku
 # git add . (dodanie wszystkiego na repo)
+
+# Tworzenie commita
+# git commit -m 'wiadomosc'
+
