fa36f2c4a99a1fd9920f278ec558159f2b6c9910
dawtom97 <daw.tomczyk1997@gmail.com>
2025-07-09 17:18:01 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+title = "Mój nowy plik"
+x = 5
+# Tworzenie lokalnego repozytorium
+# git init
+
+# Sprawdzenie statusu plików
+# git status
+
+# Dodawanie pliku do repozytorium - lokalnego
+# git add nazwa_pliku
+# git add . (dodanie wszystkiego na repo)
